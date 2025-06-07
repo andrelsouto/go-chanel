8c8861663eca5120a9248d874731c159f9b784b3
André Luiz <a.luiz.souto@gmail.com>
2025-06-07 22:07:58 +0000

    Project

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+const viaCepApi = "https://viacep.com.br/ws/%s/json/"
+const brasilApi = "https://brasilapi.com.br/api/cep/v1/%s"
+
+type Request struct {
+	url string
+	cep string
+	ch  chan<- string
+}
+
+func main() {
+
+	cep := os.Args[1:][0]
+
+	chViaCep := make(chan string)
+	chBrasilApi := make(chan string)
+
+	viaCepRequest := Request{
+		url: viaCepApi,
+		cep: cep,
+		ch:  chViaCep,
+	}
+	brasilApiRequest := Request{
+		url: brasilApi,
+		cep: cep,
+		ch:  chBrasilApi,
+	}
+
+	go getCep(viaCepRequest)
+	go getCep(brasilApiRequest)
+
+	select {
+	case response := <-chViaCep:
+		fmt.Println("Response from ViaCEP: ", response)
+	case response := <-chBrasilApi:
+		fmt.Println("Response from BrasilApi: ", response)
+	case <-time.After(time.Second):
+		fmt.Println("Timeout: No response received within 1 second")
+	}
+
+}
+
+func getCep(request Request) error {
+	url := fmt.Sprintf(request.url, request.cep)
+	req, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer req.Body.Close()
+	jsonByte, err := io.ReadAll(req.Body)
+	if err != nil {
+		return err
+	}
+	request.ch <- string(jsonByte)
+	return nil
+}
